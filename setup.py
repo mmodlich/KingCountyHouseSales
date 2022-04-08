fa887ca631dc8712dd8de43e1e0c2d63410c4b04
Mitch Modlich <mitch.modlich@gmail.com>
2022-04-08 13:58:50 -0400

    created file structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Develop a regression-based pricing model from features of single family house sales in King County, Washihington',
+    author='Mitch Modlich',
+    license='MIT',
+)
